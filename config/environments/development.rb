e62ece35e6f57e8fb865df8e5001c86251ef36b6
Vega Ramirez <jorgevp5@gmail.com>
2015-08-05 18:03:37 -0400

    Devise installation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+    #Required for Heroku
+  # Note to set this to your actual host
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
 end
