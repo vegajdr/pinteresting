7475ad81ffa7961ac61f5c8d2b91d368754ea7c3
Vega Ramirez <jorgevp5@gmail.com>
2015-08-19 18:37:13 -0400

    Day 30

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,6 @@ class Pin < ActiveRecord::Base
 	
 	has_attached_file :image, :styles => { :medium => "700x700>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+	validates :image, presence: true
 end
