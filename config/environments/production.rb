91df5f53c47938adeecb8b8bdb32c97de2402936
vegajdr <jorgevp5@gmail.com>
2016-01-22 17:17:31 +0000

    Added s3_region variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,12 +83,13 @@ Rails.application.configure do
 
 
   #Sets Paperclip to upload images to Amazon S3
-config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+    :s3_region => 'us-east-1',
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
 }
-end
+end 
